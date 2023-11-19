7172435b2d5e6a94187b9d089957df40388d65b6
Waqas Ahmed <waqas.ghaloo@gmail.com>
2023-11-19 11:01:01 +0300

    updated readme and connection file

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # azure.mlpipeline
 
+[There are two CLIs provided by Azure 'az' and 'azd'](https://devblogs.microsoft.com/azure-sdk/introducing-the-azure-developer-cli-a-faster-way-to-build-apps-for-the-cloud/)
+##### The Azure Developer CLI (azd) is intended to be complementary to the Azure CLI (az). With the Azure Developer CLI, we keep the commands you use high-level and mapped to stages your development workflow. By keeping things high-level some of the nitty gritty control plane management pieces get abstracted away so that you can focus on writing code. If you want to do more fine-tuned management of Azure resources, your best bet is the Azure CLI. With azd, you get prebuilt templates and focus on writing code rather than building completing application from scratch.
+
+[Learning Series Youtube](https://www.youtube.com/playlist?list=PLlrxD0HtieHjDop2DtiCmwTTcrlwKAVHE)
+
+#### Authenticte
+[Install Azure CLI Developer azd on VSCode](https://learn.microsoft.com/en-us/azure/developer/azure-developer-cli/install-azd?tabs=winget-windows%2Cbrew-mac%2Cscript-linux&pivots=os-windows)
+##### Authenticate using 'azd auth login' and credentials will be brought to DefaultCredentials class
+
+[Installation of az CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli)
+
 #### Setup Enviroment
 [Setup development enviroment of azure ml](https://learn.microsoft.com/en-us/azure/machine-learning/how-to-configure-environment?view=azureml-api-2#local-and-dsvm-only-create-a-workspace-configuration-file)
 
--- connection.py
+++ connection.py
@@ -1,11 +1,45 @@
+import os
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
+import json
+
+
+
+
+def construct_path(subdirectory = "src"):
+    # Get the absolute path to the script's directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the absolute path to the config file
+    config_path = os.path.join(script_dir,subdirectory, "config.json")
+
+    return config_path
+
+configpath = construct_path()
+
+def read_configjson(config_path, key = "workspace_name"):
+    # Read the configuration from the JSON file
+    # Read the contents of the JSON file as a string
+    with open(config_path, "r") as file:
+        config_json_string = file.read()
+
+    # Parse the JSON string
+    config_data = json.loads(config_json_string)
+    # Access the values using the key
+    workspace_name = config_data.get(key)
+
+    return workspace_name
 
 
 # Creating a client of Azure ML and loading configuration from config.json
-ml_client =  client = MLClient.from_config(
-       credential=DefaultAzureCredential(),
-       file_name=r"..\azure.mlpipeline\src\config.json",
+ml_client = MLClient.from_config(
+    # Reading DefaultAzureCredential is possible when 'azd auth login' is done
+    credential = DefaultAzureCredential(),   
+    path = construct_path()
    )
 
-print(ml_client)
+# Verify that the handle works correctly.  
+# If you ge an error here, modify your SUBSCRIPTION, RESOURCE_GROUP, and WS_NAME in the previous cell.
+ws = ml_client.workspaces.get(read_configjson(configpath))
+print(ws.location,":", ws.resource_group)
+
